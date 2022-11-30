0d39d95386816cc3e682ffa0b9f3f6099fa60666
Arianna Bunn <ajbunny12@gmail.com>
2022-11-30 13:25:15 -0500

    Top Calculator express application created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000
 const fs = require('fs');
 
+//Greetings
 app.get('/greeting',(req, res) => {
     res.send('Hello, Stranger');
 })
@@ -10,6 +11,14 @@ app.get('/greeting',(req, res) => {
 app.get('/greeting/:name', (req, res) => {
     res.send('Whadddddup' + req.params.name + '!' + 'It\'s so great to see you!')
 })
+
+//Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+     const bigTip = (req.params.total * (req.params.tipPercentage / 100))
+    res.send('Your tip is' + ' ' + bigTip);
+})
+
+
 
 app.listen(port, () => {
     console.log('listening');
