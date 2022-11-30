241e8f7bfe71d580257ddf36afdf656df489098f
Arianna Bunn <ajbunny12@gmail.com>
2022-11-30 13:45:13 -0500

    Magic 8 ball express application created

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send('Your tip is' + ' ' + bigTip);
 })
 
+//Magic 8 Ball
+
+app.get('/magic/:phrase', (req, res) =>{
+    const eightball = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+    
+    let fortune = eightball[Math.floor(Math.random() * eightball.length)];
+    res.send(req.params.phrase + `<h1> ${fortune}</h1>`)
+
+
+
+})
 
 
 app.listen(port, () => {
